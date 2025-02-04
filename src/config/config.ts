4bfe8b313189c40726d45a28417b30cbc1b41e54
TokyoToshi <rizz.muffin24@gmail.com>
2025-02-05 07:56:24 +0900

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 import { config as dotenv } from "dotenv";
 dotenv();
 
-export const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/PositionChecher";
+export const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/PositionChecker";
 
 export const PORT = process.env.PORT || 3000;
