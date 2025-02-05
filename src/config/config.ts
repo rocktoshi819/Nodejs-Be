e88c8aeccd6b6b539b0c17d4cb492c1c1892e095
TokyoToshi <rizz.muffin24@gmail.com>
2025-02-06 02:42:30 +0900

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 import { config as dotenv } from "dotenv";
 dotenv();
 
-export const MONGO_URL = "mongodb+srv://muffin:RIZZ@cluster0.xf6zg.mongodb.net/PositionChecher";
+export const MONGO_URL = "mongodb+srv://muffin:rizz@cluster0.xf6zg.mongodb.net/PositionChecker";
 
 export const PORT = 5000;
