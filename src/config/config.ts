4faaf0c5270d549c2463533b5fbd79128ae76ee8
rocktoshi819 <rizz.muffin24@gmail.com>
2025-02-05 10:31:34 +0900

    fix: update mongourl

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 import { config as dotenv } from "dotenv";
 dotenv();
 
-export const MONGO_URL = "mongodb+srv://muffin:RIZZ@cluster0.xf6zg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+export const MONGO_URL = "mongodb+srv://muffin:RIZZ@cluster0.xf6zg.mongodb.net/PositionChecher";
 
 export const PORT = 5000;
