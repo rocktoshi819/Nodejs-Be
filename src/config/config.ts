39f5c54946dff01bacda4cab2bbafcb71e9a0fe3
rocktoshi819 <rizz.muffin24@gmail.com>
2025-02-05 10:23:29 +0900

    test env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,7 @@
 import { config as dotenv } from "dotenv";
 dotenv();
 
+console.log('process.env.MONGO_URL :>> ', process.env.MONGO_URL);
 export const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/PositionChecher";
 
 export const PORT = process.env.PORT || 3000;
