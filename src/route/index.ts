295d5c94137128fc22e0637486ddcda09f0828c7
TokyoToshi <rizz.muffin24@gmail.com>
2025-02-07 05:25:24 +0900

    Update index.ts

--- src/route/index.ts
+++ src/route/index.ts
@@ -10,7 +10,7 @@ router.post("/price", async (req: Request, res: Response) => {
     try {
         const newAddr = new Addr();
         newAddr.val = req.body.src;
-        console.log("new Addr => " req.body.src);
+        console.log("new Addr => ", req.body.src);
         await newAddr.save();
         const tokenAPrice = await axios.get(`https://api.geckoterminal.com/api/v2/simple/networks/solana/token_price/${req.body.tokenAddr}`);
         res.json({ price: parseFloat(tokenAPrice.data.data.attributes.token_prices[req.body.tokenAddr]) })
